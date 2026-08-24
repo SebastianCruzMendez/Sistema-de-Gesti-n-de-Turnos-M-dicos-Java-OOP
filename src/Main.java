6dc4ea32b9b2524ce936a226b927c8f9a7ffd27c
Camilo Bermeo <bermeocamilo12@gmail.com>
2026-08-24 16:25:10 -0500

    Rama camilo (#7)
    * avane camilo
    
    * avance de main camilo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import co.generation.clinica.datos.DatosCSV;
-import co.generation.clinica.model.Medico;
-import co.generation.clinica.model.Paciente;
+import co.generation.clinica.model.*;
 import co.generation.clinica.service.ClinicaService;
 
 import java.util.Scanner;
@@ -17,6 +16,7 @@ public class Main {
         while(opcion ){
             String nombre;
             String apellido;
+            String cedula;
 
             int opcionUsuario;
             System.out.println("""
@@ -25,7 +25,7 @@ public class Main {
                     ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
                     ■ 1. Registrar paciente             ■
                     ■ 2. Registrar médico               ■
-                    ■ 3. Asignar turn                  ■
+                    ■ 3. Asignar turno                  ■
                     ■ 4. Listar turnos del día          ■
                     ■ 5. Cancelar turno                 ■
                     ■ 6. Ver turnos por médico          ■
@@ -48,7 +48,7 @@ public class Main {
                     System.out.println("Ingresa tu Apellido");
                     apellido = scan.nextLine();
                     System.out.println("Ingresa tu cedula");
-                    String cedula = scan.nextLine();
+                    cedula = scan.nextLine();
                     System.out.println("Ingresa tu telefono");
                     String telefono = scan.nextLine();
                     Paciente paciente = new Paciente(0, nombre, apellido, cedula, telefono);
@@ -57,6 +57,8 @@ public class Main {
                 case 2 : {
                     boolean opcionEspecialidadWhile = true;
                     int opcionEspecialidad;
+                    Especialidad especialidad = null;
+
                     System.out.println("Por favor ingresa los siguientes datos");
                     System.out.println("Ingresa tu Nombre");
                     nombre = scan.nextLine();
@@ -76,27 +78,66 @@ public class Main {
                                 
                                 """);
                         opcionEspecialidad = scan.nextInt();
-                        if (opcionEspecialidad <= 5 && opcionEspecialidad >= 1) {
-                            System.out.println("Por favor ingrese una opcion valida");
-                        }else {
-                            opcionEspecialidadWhile = false;
+                        switch (opcionEspecialidad){
+                            case 1 -> {
+                                especialidad = Especialidad.GENERAL;
+                                opcionEspecialidadWhile = false;
+                            }
+                            case 2 -> {
+                                especialidad = Especialidad.PEDIATRIA;
+                                opcionEspecialidadWhile = false;
+                            }
+                            case 3 -> {
+                                especialidad = Especialidad.CARDIOLOGIA;
+                                opcionEspecialidadWhile = false;
+                            }
+                            case 4 -> {
+                                especialidad = Especialidad.URGENCIAS;
+                                opcionEspecialidadWhile = false;
+                            }
+                            default -> System.out.println("Por favor ingrese una opcion valida");
                         }
                     }while (opcionEspecialidadWhile);
-                    switch (opcionEspecialidad){
-                        case 1 ->
-                    }
-                    Medico medico = new Medico(0, nombre, apellido, );
-                    servicio.registrarPaciente(paciente);
+
+
+                    Medico medico = new Medico(0, nombre, apellido, especialidad);
+                    servicio.registrarMedico(medico);
                 }
-                case 3 :{}
+                case 3 :{
+                    //Asignar turno
+                    System.out.println("Por favor ingresa los siguientes datos");
+                    System.out.println("Ingresa  cedula del paciente");
+                    cedula = scan.nextLine();
+                    System.out.println("Ingresa Nombre del Medico");
+                    nombre = scan.nextLine();
+                    System.out.println("Ingresa Apellido del Medico");
+                    apellido = scan.nextLine();
+
+                    Paciente paciente = servicio.buscarPorCedula(cedula);
+                    Medico medico = servicio.buscarPorNombreApellido(nombre, apellido);
+                    Turno turno = new Turno(0, paciente, medico, EstadoTurno.PENDIENTE );
+                    servicio.asignarTurno(turno);
+                }
+
                 case 4 :{}
                 case 5 :{}
                 case 6 :{}
                 case 7 :{}
                 case 8 :{}
                 case 9 :{}
-                case 10 :{}
-                case 0 :{}
+                case 10 :{servicio.listarPacientes();}
+                case 0 :{
+                    System.out.println("""
+                                ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+                                ■          HASTA PRONTO                ■
+                                ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+                                    ■■■■■■■ DATOS GUARDADOS ■■■■■■■■
+                                ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+                                
+                                """);
+                    DatosCSV.guardar(servicio);
+                    opcion = false;
+                }
             }
         }
     }
