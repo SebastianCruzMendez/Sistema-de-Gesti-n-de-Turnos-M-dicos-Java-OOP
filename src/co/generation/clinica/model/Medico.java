2cf84556713c1e12ec9aeb14bf7a25b33f599635
Andres <wandresguzman@hotmail.com>
2026-08-24 10:55:40 -0500

    creacion-Model

--- src/co/generation/clinica/model/Medico.java
+++ src/co/generation/clinica/model/Medico.java
@@ -1,4 +1,52 @@
 package co.generation.clinica.model;
 
-public class Medico {
+import co.generation.clinica.interfaces.Registrable;
+
+public class Medico implements Registrable {
+
+    int id;
+    String nombre;
+    String apellido;
+    Especialidad especialidad;
+
+    public Medico(int id, String nombre, String apellido, Especialidad especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+
 }
--- src/co/generation/clinica/model/Paciente.java
+++ src/co/generation/clinica/model/Paciente.java
@@ -1,4 +1,70 @@
 package co.generation.clinica.model;
 
-public class Paciente {
+import co.generation.clinica.interfaces.Registrable;
+
+public class Paciente implements Registrable {
+
+    int id;
+    private String  nombre;
+    private String cedula;
+    private String apellido;
+    private String telefono;
+
+    public Paciente(int id, String nombre, String cedula, String apellido, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+public Paciente registrar(String nombre, String cedula, String apellido, String telefono) {
+
+        return new Paciente(0, nombre, cedula, apellido, telefono);
+}
+
+public Paciente reconstruir(int id, String nombre, String cedula, String apellido, String telefono) {
+        return new Paciente(id, nombre, cedula, apellido, telefono);
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
 }
